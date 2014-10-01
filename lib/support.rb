aceb49f6573c017f7c29d86fb677bdd0cc3103a0
Joseph Wilk <joe@josephwilk.net>
2014-10-01 11:02:16 +0200

    silence + solo

--- lib/support.rb
+++ lib/support.rb
@@ -3,16 +3,43 @@ def csample(name)
   "#{root}/#{name}"
 end
 
-def define_loop(name, &block)
+def with_echo(args, &block)
+  with_fx :echo do
+    block
+  end
+end
+
+def live_loop(name, &block)
+  raise "live_loop must be called with a code block" unless block
+  (@__live_cache ||= []) << name
+  @__live_cache = @__live_cache.uniq
+
   define(name, &block)
-  in_thread(name: name) { loop{self.send(name)} }
+
+  in_thread(name: name) do
+    loop do
+      cue name
+      send(name)
+    end
+  end
+end
+
+def silence(name)
+  if @user_methods.method_defined?(name)
+    define(name) do |*args|
+      sleep 1
+    end
+  else
+    raise "could not find #{name}"
+  end
 end
 
 def solo(name)
-  #{:backing_highlights=>1, :backing_highlights2=>4}
-  #@named_subthreads
-  #HERE LIES EVIL
-  #quiet_threads.map(&kill)
+  if @user_methods.method_defined?(name)
+    (@__live_cache - [name]).map{|n| silence(n)}
+  else
+    raise "could not find #{name}"
+  end
 end
 
 def fadeout
