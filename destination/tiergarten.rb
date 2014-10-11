64d7cacc44abc6aae26861079af2d533f88e20a3
Joseph Wilk <joe@josephwilk.net>
2014-10-11 11:21:27 +0100

    Happy birthday with sonic pi

--- destination/tiergarten.rb
+++ destination/tiergarten.rb
@@ -1,7 +1,3 @@
-#             _                   \:o/     π=-      π=-
-#_|_ o  _  __(_| _  ___|_ _ __     █       π=-   π   π=-   π=  
-# |_ | (/_ | __|(_| |  |_(/_| |  .||.     π=-  π=-  π=-
-#
 require "/Users/josephwilk/Workspace/repl-electric/sonic-pi/lib/support"
 soporano_root = "/Users/josephwilk/Workspace/music/samples/soprano/Samples"
 sop_ah1_s = "#{soporano_root}/Sustains/Ah p/vor_sopr_sustain_ah_p_01.wav"
@@ -36,33 +32,26 @@ drum_14_s             = csample "c14.aif"
 drum_2_s              = csample "c2.aif"
 gutteral_wobble_s     = csample "blip.wav"
 
-bar    = beat_dur / 4.0
-half   = beat_dur / 2.0
-eighth = beat_dur / 8.0
-quart  = beat_dur / 16.0
-
 clacker_dur   = sample_duration(clacker_s)
 beat_dur      = sample_duration(beat_s)
 halixic_dur   = sample_duration(halixic_s)
 eery_ratio    = sample_duration(eery_vocals_s) / beat_dur
 clacker_ratio = (sample_duration clacker_s) / beat_dur
 
+bar    = beat_dur / 4.0
+half   = beat_dur / 2.0
+eighth = beat_dur / 8.0
+quart  = beat_dur / 16.0
+
 live_loop_it :backing_highlights do
   sync :drums
-  sample [sixg_s, sixa_s, sixd_s].choose, rate: [1/2.0, 1/4.0, 1/8.0].choose
-  sample halixic_s, rate: 0.2
-  sample halixic_s
-  #  3if dice(6) > 1
-  with_fx :slicer  do
-    options = [1.0 -1.0]
-    sample halixic_s, rate: options.choose
-    sleep half
-    sample halixic_s, rate: options.choose
-    sleep bar
-    sample halixic_s, rate: options.choose
-    sleep bar
-    sample halixic_s, rate: options.choose
-    sleep half
+  sleep bar*2
+  with_fx :reverb do
+    with_fx :rlpf do
+      with_fx :ixi_techno, phase: bar do
+        sample [sixg_s, sixa_s, sixd_s].choose, rate: [eery_ratio].choose, amp: 0.1, pan: [0.1, -0.1].choose
+      end
+    end
   end
 end
 
@@ -86,7 +75,7 @@ live_loop_it :drums2 do
     sample drum_2_s, amp: 0.7
     #sample c13
     sync :the_snare
-    sample drum_2_s, amp: 0.7
+    sample drum_2_s, amp: 0.8
   end
 end
 
@@ -100,9 +89,9 @@ end
 
 live_loop_it :drums do |inc|
   tempo = [60*2].choose
-  with_fx :lpf, cutoff: lambda{ 0 }  do
+  with_fx :lpf, cutoff: lambda{ 100 }  do
     with_bpm tempo do
-      sleep_rate = 8.0
+      sleep_rate = 2.0
       sample :drum_heavy_kick, rate: 0.8
       if sleep_rate == 8.0
         s = [drum_2_s].choose
@@ -120,7 +109,7 @@ live_loop_it :drums do |inc|
         cue :the_snare
       else
         with_fx :rlpf do
-#          sample beat_s, pan: lambda{rrand(-0.8,0.8)}, rate: [1].choose
+          #          sample beat_s, pan: lambda{rrand(-0.8,0.8)}, rate: [1].choose
           sleep beat_dur/sleep_rate
           cue :the_snare
           sleep beat_dur/sleep_rate
@@ -165,8 +154,8 @@ live_loop_it :eery_vocals do |n|
     rate = 1
   end
   sync :drums
-  #with_fx(:slicer, phase: lambda{[eery_ratio, beat_dur/8, beat_dur/4].choose}) do sample eery_vocals_s, rate: rate, amp: vol+0.2 end
-  #with_fx :lpf, cutoff: 100 do  sample eery_vocals_s, rate: eery_ratio/4.0, amp: vol end
+  with_fx(:slicer, phase: lambda{[eery_ratio, beat_dur/8, beat_dur/4].choose}) do sample eery_vocals_s, rate: rate, amp: vol+0.2 end
+  with_fx :lpf, cutoff: 100 do  sample eery_vocals_s, rate: eery_ratio/4.0, amp: vol end
   sample eery_vocals_s, rate: (eery_ratio/4.0), amp: vol
   #sample eery_vocals_s, rate: (eery_ratio/8.0), amp: vol
 
@@ -179,18 +168,18 @@ live_loop_it :deeper_vocals do |n|
   rate = [0.7, -0.7].choose
   fxs =[:echo, :reverb, :reverb, :reverb, :reverb, :lpf]
   with_fx fxs.choose do
-    sample whisper_s, amp: 2.5, rate: rate, attack_level: 0.0
+    sample whisper_s, amp: 1.0, rate: rate, attack_level: 0.0
   end
-  
+
   sleep beat_dur
 
   #POWER LIES HERE
-  if n % 4 == 0
-  with_fx :echo do
-    with_fx :reverb do
-      sample eery_vocals_s, start: 0.0, finish: 0.1, amp: 5, rate: eery_ratio/2.0
+  if n % 8 == 0
+    with_fx :echo do
+      with_fx :reverb do
+        sample eery_vocals_s, start: 0.0, finish: 0.1, amp: 1.2, rate: eery_ratio/2.0
+      end
     end
-  end
   end
   #sample whisper_s, start: 0.2, finish: 1.0, rate: 1, amp: 1
 end
@@ -247,7 +236,7 @@ live_loop_it :floating_voices do |what_n|
         with_fx :slicer, phase: quart do
           sample ah_candidate, rate: eery_ratio, pan: -0.25, release: 0.01, decay: 0.01
         end
-        
+
         #If we are feeling *brave*, lets try some overlap
         sleep beat_dur/2.0
         with_fx :slicer, phase: [quart/2, quart].choose  do
@@ -259,18 +248,18 @@ live_loop_it :floating_voices do |what_n|
 end
 
 #IN THE BEGINNING
-silence :whispers_wind
+#silence :whispers_wind
 silence :ethereal
-silence :floating_voices
+#silence :floating_voices
 silence :drums3
 #silence :drums
-silence :drums2
-silence :deeper_vocals
-silence :eery_vocals
-silence :glitch
+#silence :drums2
+#silence :deeper_vocals
+#silence :eery_vocals
+#silence :glitch
 silence :zoom
-silence :ambience
-silence :backing_highlights
+#silence :ambience
+#silence :backing_highlights
 silence :backing_melody
 
 #set_volume! 0
