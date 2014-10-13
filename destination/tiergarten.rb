9100cc5b9c43dcd8ebe03b07e597fcbcaf3c055a
Joseph Wilk <joe@josephwilk.net>
2014-10-13 09:01:30 +0100

    share the samples

--- destination/tiergarten.rb
+++ destination/tiergarten.rb
@@ -3,38 +3,7 @@
 # |_ | (/_ | __|(_| |  |_(/_| |  .||.     π=-  π=-  π=-
 #
 require "/Users/josephwilk/Workspace/repl-electric/sonic-pi/lib/support"
-soporano_root = "/Users/josephwilk/Workspace/music/samples/soprano/Samples"
-sop_ah1_s = "#{soporano_root}/Sustains/Ah p/vor_sopr_sustain_ah_p_01.wav"
-sop_ah2_s = "#{soporano_root}/Sustains/Ah p/vor_sopr_sustain_ah_p_02.wav"
-sop_ah3_s = "#{soporano_root}/Sustains/Ah p/vor_sopr_sustain_ah_p_03.wav"
-sop_ah4_s = "#{soporano_root}/Sustains/Ah p/vor_sopr_sustain_ah_p_04.wav"
-halixic_s             = csample "halixic.wav"
-clacker_s             = csample "clacker_rhythm.wav"
-eery_vocals_s         = csample "hypnoticsynth.wav"
-zoom_s                = csample "nano_blade_loop.wav"
-beat_s                = csample "crunchy_beat.aif"
-whisper_s             = csample "whisperloop.wav"
-ethereal_femininity_s = csample "ethereal_femininity.wav"
-sixg_s                = csample "120bpmacantholabrus6s_g.wav"
-sixa_s                = csample "120bpmacantholabrus6s_a.wav"
-sixd_s                = csample "120bpmacantholabrus6s_d.wav"
-fourg_s               = csample "120bpmacantholabrus4s_g.wav"
-h_s                   = csample "120bpm2smagnhildhh.wav"
-d_s                   = csample "120bpm2smagnhildbd.wav"
-feedback_s            = csample "feedback21.wav"
-#house_lead_s         = csample "128-bpm-house-lead-fx.wav"
-nasal_s               = csample "183669__alienxxx__loop2-009-nasal-120bpm.wav"
-arp_s                 = csample "20341__djgriffin__trippyarp120bpm.aif"
-epsilon_four_s        = csample "249178__gis-sweden__120bpmepsilon4s-g.wav"
-epsilon_six_s         = csample "249177__gis-sweden__120bpmepsilon6s-d.wav"
-meta_six_s            = csample "249179__gis-sweden__120bpmeta6s-g.wav"
-skappa_s              = csample "249870__gis-sweden__120bpm10skappa-g.wav"
-#fourg_s              = csample "120bpmabramis4s_g.wav"
-voc_s                 = csample "150399__mikobuntu__voc-formant9.wav"
-drum_13_s             = csample "c13.aif"
-drum_14_s             = csample "c14.aif"
-drum_2_s              = csample "c2.aif"
-gutteral_wobble_s     = csample "blip.wav"
+require "/Users/josephwilk/Workspace/repl-electric/sonic-pi/lib/samples"
 
 clacker_dur   = sample_duration(clacker_s)
 beat_dur      = sample_duration(beat_s)
@@ -223,20 +192,21 @@ live_loop_it :ethereal do |n|
 end
 
 live_loop_it :whispers_wind do
+
   with_fx :echo do
     with_fx :lpf do
-    s = whisper_s
-    pan_amps = [[0.6, 0.55, 0.4], [0.6, 0.55, 0.4].reverse].choose
-    pan_dir = [[1, 0.0, -1], [-1, 0, 1]].choose
+      s = whisper_s
+      pan_amps = [[0.6, 0.55, 0.4], [0.6, 0.55, 0.4].reverse].choose
+      pan_dir = [[1, 0.0, -1], [-1, 0, 1]].choose
 
-    sync :drums
-    sleep eery_ratio/4.0
-    sample s, rate: -1.0, pan: pan_dir[0], amp: pan_amps[0]
-    sleep 1
-    sample s, rate: 1.0, pan: pan_dir[1], amp:  pan_amps[1]
-    sleep 1
-    sample s, rate: -1.0, pan: pan_dir[2], amp:  pan_amps[2]
-    sleep  beat_dur*4
+      sync :drums
+      sleep eery_ratio/4.0
+      sample s, rate: -1.0, pan: pan_dir[0], amp: pan_amps[0]
+      sleep 1
+      sample s, rate: 1.0, pan: pan_dir[1], amp:  pan_amps[1]
+      sleep 1
+      sample s, rate: -1.0, pan: pan_dir[2], amp:  pan_amps[2]
+      sleep  beat_dur*4
     end
   end
 end
@@ -251,29 +221,30 @@ live_loop_it :floating_voices do |what_n|
         #sample sop_ah1_s
         sample eery_vocals_s, start: 0.4, finish: 0.5, amp: 2, rate: [eery_ratio/1.5, eery_ratio/1.5, eery_ratio].choose
 
-        #ah_candidate = [sop_ah1_s,sop_ah2_s,sop_ah3_s].choose
-        #with_fx :slicer, phase: quart do
-        #  sample ah_candidate, rate: eery_ratio, pan: -0.25, release: 0.01, decay: 0.01
-        #end
+        ah_candidate = [sop_ah1_s,sop_ah2_s,sop_ah3_s].choose
+        with_fx :slicer, phase: quart do
+          sample ah_candidate, rate: eery_ratio, pan: -0.25, release: 0.01, decay: 0.01
+        end
 
         #If we are feeling *brave*, lets try some overlap
-        #sleep beat_dur/2.0
-        #with_fx :slicer, phase: [quart/2, quart].choose  do
-        #  sample ah_candidate, rate: eery_ratio/[1.5, 2.0].choose, pan: 0.25, release: 0.01, decay: 0.01
-        #end
+        sleep beat_dur/2.0
+        with_fx :slicer, phase: [quart/2, quart].choose  do
+          sample ah_candidate, rate: eery_ratio/[1.5, 2.0].choose, pan: 0.25, release: 0.01, decay: 0.01
+        end
       end
     end
   end
 end
 
 #IN THE BEGINNING
-silence :whispers_wind
-silence :ethereal
-silence :floating_voices
-silence :drums3
-silence :drums2
+#silence :whispers_wind
+#silence :ethereal
+#silence :floating_voices
+#silence :drums3
+
+#silence :drums2
 silence :deeper_vocals
-silence :eery_vocals
+#silence :eery_vocals
 silence :glitch
 silence :zoom
 silence :ambience
