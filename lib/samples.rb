3f6d6dda506bac5c166f76c37cb41318b28646fa
Joseph Wilk <joe@josephwilk.net>
2014-10-17 20:06:36 +0200

    joe learns ruby again

--- lib/samples.rb
+++ lib/samples.rb
@@ -1,29 +1,109 @@
 module S
-def self.halixic              csample "halixic.wav"; end
-def self.clacker              csample "clacker_rhythm.wav"; end
-def self.eery_vocals          csample "hypnoticsynth.wav"; end
-def self.zoom                 csample "nano_blade_loop.wav"; end
-def self.beat                 csample "crunchy_beat.aif"; end
-def self.whisper              csample "whisperloop.wav"; end
-def self.ethereal_femininity  csample "ethereal_femininity.wav"; end
-def self.sixg                 csample "120bpmacantholabrus6s_g.wav"; end
-def self.sixa                 csample "120bpmacantholabrus6s_a.wav"; end
-def self.sixd                 csample "120bpmacantholabrus6s_d.wav"; end
-def self.fourg                csample "120bpmacantholabrus4s_g.wav"; end
-def self.h                    csample "120bpm2smagnhildhh.wav"; end
-def self.d                    csample "120bpm2smagnhildbd.wav"; end
-def self.feedback             csample "feedback21.wav"; end
-def self.house_lead          csample "128-bpm-house-lead-fx.wav"; end
-def self.nasal                csample "183669__alienxxx__loop2-009-nasal-120bpm.wav"; end
-def self.arp                  csample "20341__djgriffin__trippyarp120bpm.aif"; end
-def self.epsilon_four         csample "249178__gis-sweden__120bpmepsilon4s-g.wav"; end
-def self.epsilonix          csample "249177__gis-sweden__120bpmepsilon6s-d.wav"; end
-def self.metaix             csample "249179__gis-sweden__120bpmeta6s-g.wav"; end
-def self.skappa               csample "249870__gis-sweden__120bpm10skappa-g.wav"; end
-def self.fourg               csample "120bpmabramis4s_g.wav"; end
-def self.voc                  csample "150399__mikobuntu__voc-formant9.wav"; end
-def self.drum_13              csample "c13.aif"; end
-def self.drum_14              csample "c14.aif"; end
-def self.drum_2               csample "c2.aif"; end
-def self.gutteral_wobble      csample "blip.wav"; end
+def self.halixic              
+  csample "halixic.wav"
+end                           
+
+def self.clacker              
+  csample "clacker_rhythm.wav"
+end                           
+
+def self.eery_vocals          
+  csample "hypnoticsynth.wav"
+end                           
+
+def self.zoom                 
+  csample "nano_blade_loop.wav"
+end                           
+
+def self.beat                 
+  csample "crunchy_beat.aif"
+end                           
+
+def self.whisper              
+  csample "whisperloop.wav"
+end                           
+
+def self.ethereal_femininity  
+  csample "ethereal_femininity.wav"
+end                           
+
+def self.sixg                 
+  csample "120bpmacantholabrus6s_g.wav"
+end                           
+
+def self.sixa                 
+  csample "120bpmacantholabrus6s_a.wav"
+end                           
+
+def self.sixd                 
+  csample "120bpmacantholabrus6s_d.wav"
+end                           
+
+def self.fourg                
+  csample "120bpmacantholabrus4s_g.wav"
+end                           
+
+def self.h                    
+  csample "120bpm2smagnhildhh.wav"
+end                           
+
+def self.d                    
+  csample "120bpm2smagnhildbd.wav"
+end                           
+
+def self.feedback             
+  csample "feedback21.wav"
+end                           
+
+def self.house_lead           
+  csample "128-bpm-house-lead-fx.wav"
+end                           
+
+def self.nasal                
+  csample "183669__alienxxx__loop2-009-nasal-120bpm.wav"
+end                           
+
+def self.arp                  
+  csample "20341__djgriffin__trippyarp120bpm.aif"
+end                           
+
+def self.epsilon_four         
+  csample "249178__gis-sweden__120bpmepsilon4s-g.wav"
+end                           
+
+def self.epsilonix            
+  csample "249177__gis-sweden__120bpmepsilon6s-d.wav"
+end                           
+
+def self.metaix               
+  csample "249179__gis-sweden__120bpmeta6s-g.wav"
+end                           
+
+def self.skappa               
+  csample "249870__gis-sweden__120bpm10skappa-g.wav"
+end                           
+
+def self.fourg                
+  csample "120bpmabramis4s_g.wav"
+end                           
+
+def self.voc                  
+  csample "150399__mikobuntu__voc-formant9.wav"
+end                           
+
+def self.drum_13              
+  csample "c13.aif"
+end                           
+
+def self.drum_14              
+  csample "c14.aif"
+end                           
+
+def self.drum_2               
+  csample "c2.aif"
+end                           
+
+def self.gutteral_wobble      
+  csample "blip.wav"
+end
 end
