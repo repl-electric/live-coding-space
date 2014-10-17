74331b79c7c1f668a1dac2c97e636de92515b221
Joseph Wilk <joe@josephwilk.net>
2014-10-17 20:02:08 +0200

    easier soporanos

---
+++ lib/soprano.rb
@@ -0,0 +1,21 @@
+module Sop
+  ROOT =  "/Users/josephwilk/Workspace/music/samples/soprano/Samples"
+
+  def self.ehp
+    sop1 = "#{ROOT}/Sustains/Yeh p/vor_sopr_sustain_eh_p_01.wav"
+    sop2 = "#{ROOT}/Sustains/Yeh p/vor_sopr_sustain_eh_p_02.wav"
+    sop3 = "#{ROOT}/Sustains/Yeh p/vor_sopr_sustain_eh_p_03.wav"
+    sop4 = "#{ROOT}/Sustains/Yeh p/vor_sopr_sustain_eh_p_04.wav"
+    [sop1, sop2, sop3, sop4]
+  end
+  
+  def self.ahp
+    sop1 = "#{ROOT}/Sustains/Ah p/vor_sopr_sustain_ah_p_01.wav"
+    sop2 = "#{ROOT}/Sustains/Ah p/vor_sopr_sustain_ah_p_02.wav"
+    sop3 = "#{ROOT}/Sustains/Ah p/vor_sopr_sustain_ah_p_03.wav"
+    sop4 = "#{ROOT}/Sustains/Ah p/vor_sopr_sustain_ah_p_04.wav"
+    [sop1, sop2, sop3]
+  end
+    
+  
+end
