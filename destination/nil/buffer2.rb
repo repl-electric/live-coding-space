3b0a3b6b7cb45319bfe435842c0c03d7c5eded0d
Joseph Wilk <joe@josephwilk.net>
2015-07-31 14:18:53 +0200

    tidying

--- destination/nil/buffer2.rb
+++ destination/nil/buffer2.rb
@@ -1,52 +1,57 @@
-_=nil
-bar = 4.0
+bar = 1.0
+_ = nil
 live_loop :exceptions do
   (knit 1,6, 2,1).tick(:time).times{sync :next}
   with_fx :distortion, amp: 0.8, mix: 0.3 do
     with_synth :beep do
-      with_fx (knit :reverb,2).tick(:fx), decay: 4.0, room: 1.0, room_lag: 2.0 do |fx_verb|
-        with_transpose([0].choose) do
-          n = play (knit :Fs3, 1, :Fs2, 1, :Fs3, 1, :Fs2, 3, _, 1,
-                    :Fs2, 7, _, 1,
-                    :Cs2, 7, _, 1,
-                    :Ds2, 7, _, 1,
-                    :As2, 7, _, 1,
-                    :Es2, 7, _, 1,
+      with_fx (knit :reverb,2).tick(:fx), decay: (knit 4.0,7, 8.0,1).tick(:de), room: 1.0 do |fx_verb|
+        n = play (knit :Fs5, 1, :Fs4, 1, :Fs5, 1, :Fs4, 3, _, 1,
+                  :Fs4, 7, _, 1,
+                  :Cs4, 7, _, 1,
+                  :Ds4, 7, _, 1,
+                  :As4, 7, _, 1,
+                  :Es4, 7, _, 1,
+                  ).tick, amp: 0.3
 
-                    ).tick, amp: 0.3
+        with_transpose(-12) do
+          with_synth(:beep){
+            play (knit :Fs4, 1, :Fs4, 1, :Fs4, 1, :Fs4, 3, _, 1,
+                  :Fs4, 7, _, 1,
+                  :Cs4, 7, _, 1,
+                  :Ds4, 7, _, 1,
+                  :As4, 7, _, 1,
+                  :Es4, 7, _, 1,).tick, amp: 0.1, attack: 0.01, cutoff: 50, res: 0.5, detune: 0.0, release: 0.01
+          }
+        end
 
-          with_transpose(0) do
-            with_synth(:beep){
-              play (knit :Fs4, 1, :Fs4, 1, :Fs4, 1, :Fs4, 3, _, 1,
-                    :Fs4, 7, _, 1,
-                    :Cs4, 7, _, 1,
-                    :Ds4, 7, _, 1,
-                    :As4, 7, _, 1,
-                    :Es4, 7, _, 1,).tick, amp: 0.2, attack: 0.01, cutoff: 60, res: 0.5, detune: 0.0
-            }
+        if dice(32) == 1
+          with_fx :pan, pan: Math.sin(vt*13)/1.5 do
+            with_fx :bitcrusher, bits: 7, sample_rate: 32000 do
+               # sample Mountain[/bow/i, /f#/i, 0],amp: 0.4
+            end
           end
+        end
 
-          if (knit :echo,2, :reverb,2).look(:fx) == :reverb
-            control fx_verb, damp: rrand(0.0,1.0)
-          end
-          sleep bar/4.0
+        if (knit :echo,2, :reverb,2).look(:fx) == :reverb
+          control fx_verb, damp: rrand(0.0,1.0)
+        end
+        sleep bar/4.0
+        control n, note: (knit :As4, 4, :B4, 4,
+                          :Cs4, 4, :B4, 4).tick
 
-          control n, note: (knit :As4, 4, :B4, 4, :Cs4, 4, :B4, 4).tick
+        if (knit :echo,2, :reverb,2).look(:fx) == :reverb
+          control fx_verb, damp: rrand(0.0,1.0)
+        end
 
-          if (knit :echo,2, :reverb,2).look(:fx) == :reverb
-            control fx_verb, damp: rrand(0.0,1.0)
-          end
+        sleep bar/4.0
 
-          sleep bar/4.0
+        if (knit :echo,2, :reverb,2).look(:fx) == :reverb
+          control fx_verb, damp: rrand(0.0,1.0)
+        end
 
-          if (knit :echo,2, :reverb,2).look(:fx) == :reverb
-            control fx_verb, damp: rrand(0.0,1.0)
-          end
-
-          sleep bar/4.0
-          if (knit :echo,2, :reverb,2).look(:fx) == :reverb
-            control fx_verb, damp: rrand(0.0,1.0)
-          end
+        sleep bar/4.0
+        if (knit :echo,2, :reverb,2).look(:fx) == :reverb
+          control fx_verb, damp: rrand(0.0,1.0)
         end
       end
     end
