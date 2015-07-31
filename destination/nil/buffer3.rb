85ab1ad45d303e59a6d4d22f1746a9bb337db3d3
Joseph Wilk <joe@josephwilk.net>
2015-07-31 22:36:55 +0200

    sane defaults for visuals

--- destination/nil/buffer3.rb
+++ destination/nil/buffer3.rb
@@ -1,1 +1,7 @@
-shader(:shader, "nil.glsl")
+["shaderview"].each{|f| load "/Users/josephwilk/Workspace/repl-electric/live-coding-space/lib/#{f}.rb"}
+
+shader(:shader, "nil.glsl") 
+shader(:uniform, "iCellCount",  1.0)
+shader(:uniform, "iCellMotion", 0.0)
+shader(:uniform, "iMesh",       1.0)
+shader(:mesh, 1.0)
