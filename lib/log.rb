2dd4e750d68bf019c6021d9ff1696263a85971a6
Joseph Wilk <joe@josephwilk.net>
2015-08-14 19:58:50 +0200

    debug fn

--- lib/log.rb
+++ lib/log.rb
@@ -1,8 +1,23 @@
 def note_inspect(n,ns="")
   n = [n] unless n.respond_to?(:map)
   if n
-    "#{ns}[#{(n||[]).map{|n| n ? note_info(n).midi_string : "_"}}]"
+    "#{ns}[#{(n||[]).map{|n| n ? for_joes_brain(note_info(n).midi_string) : "_"}}]"
   else
     "#{ns}[ _ ]"
+  end
+end
+
+def for_joes_brain(n)
+  case n
+    when /^Ab/
+      n.gsub("Ab","Gs")
+    when /^Bb/
+      n.gsub("Bb", "As")
+    when /^Eb/
+      n.gsub("Eb", "Ds")
+    when /^F\d/
+      n.gsub("F", "Es")
+  else
+    n
   end
 end
