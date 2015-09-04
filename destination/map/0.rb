61d691f5b8402e5f30842a6adfe8cb91bd25ad50
Joseph Wilk <joe@josephwilk.net>
2015-09-04 15:34:31 +0200

    a new direction

--- destination/map/0.rb
+++ destination/map/0.rb
@@ -1,155 +1,128 @@
-["experiments", "log", "shaderview"].each{|f| load "/Users/josephwilk/Workspace/repl-electric/live-coding-space/lib/#{f}.rb"}
-set_volume! 1.0
+["experiments"].each{|f| load "/Users/josephwilk/Workspace/repl-electric/live-coding-space/lib/#{f}.rb"}
 _=nil
+hold     = (ring *%w{1  0 0 0 0 0 0 0 0 0   0  1  0   1 0  0}.map(&:to_i))
+tranpose = (ring *%w{-5 0 0 0 -5 0 0 0 0 -12 0 -2 -12 -2 0 -12}.map(&:to_i))
+velocity = (ring *%w{100 100 49 100 118 78 110 114 100 127 100 127 115 100 100 100}.map(&:to_i))
 
-#@s = MountainLoop[/.*/,12]
-#use_sample_bpm
-
-define :note_to_sample do |n|
-  note_info(n).midi_string.gsub("s", "#").gsub("Eb", "D#").gsub("Ab", "G#")
+live_loop :apeg do
+  use_synth :saw
+  root = (knit :Fs3, 16, :As3, 16, :Ds3, 16).tick(:notes)
+  
+  with_fx(:distortion, mix: rrand(0.0,1.0)){
+    with_synth(:hollow){
+      play (knit :Fs4,1, :Cs4,1,:Fs3,1, :Cs4,1,:Fs3,1, :Cs4,1, :Fs3,1, :Es4,1,
+                 :Fs4,1, :Cs4,1,:Fs3,1, :Cs4,1,:Fs3,1, :Cs4,1, :Fs3,1, :Es4,1,
+                 :Fs4,1, :Cs4,1,:Fs3,1, :Cs4,1,:Fs3,1, :Cs4,1, :Fs3,1, :Es4,1,
+                 :Fs4,1, :Cs4,1,:Fs3,1, :Cs4,1,:Fs3,1, :Cs4,1, _,1,    :As4,2
+            ).tick(:thing), attack: 4.0, release: 0.1
+    }
+  }
+  with_transpose(-24){
+    with_synth(:dsaw){
+      play (knit root,1, _, 15).tick(:bass), release: 1.5, decay: 2.5, amp: 0.5, cutoff: 70
+    }
+    with_synth(:prophet){
+      play (knit root,1, _, 15).look(:bass), release: 1.5, decay: 2.0, amp: 0.2, cutoff: 70
+    }
+  }
+  sleep 0.25
 end
 
-# F♯, G♯, A, B, C♯, D, and E
-#use_sample_bpm Fraz[/F#m/i, 0]
+with_fx(:reverb, room: 1.0, mix: 1.0){ |r_fx|
+live_loop :chorus do
+  2.times{sync :apeg}
+    with_synth(:hollow){
+notes = (knit 
+         chord_degree(1, :fs3, :major)[1..-1], 8,
+         chord_degree(3, :fs3, :major)[1..-1], 8,
+         chord(:Ds3, "m")[1..-1], 8,
 
-r = 1/8.0
-
-live_loop :arp do
-  with_synth :prophet do
-    n = (knit
-         chord(:Fs3, ['m'].choose),     64,
-         chord(:Fs3, ['m+5'].choose),   32,
-         chord(:Fs3, ['m7+5'].choose, invert: -1),  16,
-         chord(:A3, :sus2, invert: 0), 32,
-
-         chord_degree(6, :Fs3, :minor, 3,  invert: -3)[0..2],8,
-         chord_degree(6, :Fs3, :minor, 3,  invert: -3),24,
-         chord_degree(6, :Fs3, :minor, 3,  invert: -4),32,
-
-         chord_degree(3, :Fs2, :minor, 3,  invert: 1),64,
-         chord_degree(4, :Fs2, :minor, 3,  invert: 2),32,
-         chord_degree(7, :Fs2, :minor, 3,  invert: 1),32,
-
-#         chord_degree(5, :Fs3, :minor, 3,  invert: -3),32,
-#         chord_degree(5, :Fs3, :minor, 3,  invert: -4),32,
-#         chord_degree(7, :Fs3, :minor,  3, invert: -2),16,
-#         chord_degree(7, :Fs3, :minor,  3, invert: -1),16,
-#         chord_degree(7, :Fs3, :minor,  3, invert: 0),32,
-#         chord(:Fs4, ['m'].choose),     8,
-#         chord(:Fs3, ['m'].choose),     8,
-#         chord(:Fs4, ['m'].choose),     8,
-#         chord(:Fs4, ['m'].choose, invert: -1),  4,
-#         chord(:Fs4, ['m'].choose, invert: -2),   4,
-).tick(:c)
-    #puts note_inspect(n)
-comment do
-    with_fx(:slicer, phase: 1/2.0, invert_phase: 1, probability: 0.5, mix: 0.0) do
-      synth :hollow, note: (knit
-                            _,3,
-                            chord(:Fs2, 'm'),2,
-                            _, 3,
-                            _, 3,
-                            chord(:Fs2, 'm7+5', invert: 1),2,
-                            _, 3
-).tick(:hc), amp: 1.0, attack: 0.01, decay: 1.0, release: 0.1, cutoff: 80
+         chord(:Fs3, 'sus4')[1..-1], 8,
+         chord(:As3, 'm+5')[1..-1], 8,
+         chord(:Ds3, "m7")[1..-1], 8,
+      )
+      4.times{control r_fx, dry: rrand(0.0,1.0) ; sleep 0.25/4.0}
+      play notes.tick(:h), amp: (knit 1.0,4, 0.0,4).tick(:amp), release: 0.5, decay: 0.5
+      puts (notes.look(:h))
+    }
 end
-    end
-
-    with_fx((knit :none,1, :reverb, 0, :none, 0).tick(:fx), decay: 1.0, phase: 1/4.0, room: 0.2) do
-      c = (knit  _,1, n[0], 3,
-                      n[-1],3, _,1,
-                      n[1],2, _,2).tick(:c) #.choose
-      with_transpose(0) do
-        #synth :prophet,  note: (ring c, _).tick , amp: 1.0, attack: 0.01, cutoff: 60
-#        synth :plucked,  note: (ring c,_,_,_).tick , amp: 2.0, attack: 0.01
-        shader(:uniform, "iCellMotion", 0.0)
-      end
-
-      with_transpose(0){
-      with_synth(:fm){
-        play (knit n,32).tick(:support), amp: 2, release: 0.2, attack: 0.1, attack_level: 0.5, detune: 12
-      }
-}
-      with_transpose(-12) do
-        with_synth(:growl) do
-          play (knit n[0],1, _,7).tick(:bass), decay: (1/2.0)*2, release: 0.1, attack: 0.1, amp: 0.6
-        end
-        with_transpose(0) do
-          with_synth(:growl) do
-            play (knit :Fs3,1,_,7).look(:bass), decay: 0.5, release: (knit 0.5,8, 0.5,8).tick(:r), attack: 0.001, amp: 0.2, detune: 12
-            if (knit n[0],1, _,7).look(:bass)
-              with_fx(:slicer, mix: 0.0, phase: 1/1.0, smooth: 0.0, probability: 0.5) do
-#                sample Heat[/Low_Pad/i, /#{note_to_sample(n[-1])[0..-2]}1/i], amp: 2.5
-                            #puts¯ note_info(n[0]).midi_string.gsub("s", "#").gsub("Eb", "D#").gsub("Ab", "G#")[0..-2]
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-  sleep 1/8.0
-end
-
-live_loop :background do
-  sample Ether[/D/i, 0], amp: 2.0, rate: 1.0, pan: (Math.sin(vt*13)/1.5)
-  32.times{sync :drum}
-end
-
-@cell_count = 0
-live_loop :drum do |e_idx|
-#  sample Heat[/F#/i,1], amp: 1.0
- # sample Heat[/C/i,1], amp: 1.0
-  puts e_idx % 16
-  if(e_idx % 4 == 0)
-    @cell_count += 1
-    shader(:uniform, "iCellCount", 2)
-  #  sample Fraz[/kick/i, [14,15]].tick(:high), rate: 1.0, amp: 10
-  end
-
-  with_fx(:bitcrusher, bits: 10, sample_rate: 30000) do
-  (ring 4).tick(:time).times{sync :arp;   shader(:uniform, "iOffBeat",  0.0)}
-  with_fx((knit :none,3, :none,1).tick(:f), mix: 0.5, decay: 0.5, phase: 1/2.0) do |x_fx|
-#      sample Frag[/kick/i, [1,1,1,1]].tick(:drums), rate: 1.0, amp: 4.0
-   #shader(:uniform, "iBeat",  1.0)
-
-  end
-  (ring 4).tick(:time).times{sync :arp;    shader(:uniform, "iBeat",  0.0)
 }
 
-#  sample Frag[/snap/i, [0,0,0,0,1,1,1,1]].tick(:snap), rate: 1.0, amp: 2.0, start: (rrand 0.0,0.1)
-  #sample Frag[/noise/i, [0,0,0,0,1,1,1,1]].tick(:snap), rate: 1.0, amp: 2.0, start: (rrand 0.0,0.1)
- # sample Fraz[/kick/i, [0,9]].tick(:snares), rate: 1.0, amp: 10.0
- # sample Fraz[/snap/i, [0,0,0,0,1,1,1,1]].tick(:snap), rate: 1.0, amp: 2.0, start: (rrand 0.0,0.1)
-  #shader(:uniform, "iOffBeat",  1.0)
+live_loop :sample_fun do
+  32.times{sync :apeg}
+#  sample Dust[/F/,3], rate: 1.0, amp: 0.5 
+  #sample Dust[/F#/,3], rate: 1.0, amp: 0.5 
+#  sample Dust[/A#/,4], rate: 1.0, amp: 1.0, cutoff: 80
+  #sample Dust[/F#/,4], rate: 1.0, amp: 0.2, cutoff: 100
+
+#  sample (ring Dust["Vapor_keys_topline",0], Dust["Vapor_keys_02"]).tick(:a) 
+#sample Dust[/TDDC_Hit_Vapor_FullMix/]
+#sample Dust[/VoxClarinet/].tick(:a)
+
+#  (32-8).times{sync :apeg}
+#  sample Dust[/TDDC_Voice_Hit/,6], amp: 1.0
+  #sample Dust[/TDDC_Voice_Hit/,11], amp: 1.0
 
 end
-e_idx+=1
+
+live_loop :coils do
+  #play (ring chord("Cs3", "M7"), chord("Fs3", "M")).tick(:chords) , release: 8, attack: 4.0
+  synth :dark_ambience, note: (ring :Es3, :Fs4).tick, release: 8, attack: 4.0, amp: 2.5
+  32.times{sync :apeg}
+  sample Ether[/coil/i, /f#/i], amp: 1.0
 end
 
-live_loop :hats do |idx|
-  sync :arp
-  m = [0.3,0.2,0.1, 0.4].choose
-  sample Fraz[/coil/i, 0], amp: 1.0 if idx % 32 == 0
+live_loop :drums do |d_idx|
+  if d_idx % 4 == 0
+    sample Fraz[/kick/i,16], amp: 0.5, start: 0.2
+  end
 
-  4.times{
-   with_fx(:bitcrusher, bits: (ramp *(7..24).to_a.reverse).tick(:fxkc)) do
-      with_fx(:reverb, room: 0.3, mix: m) do
-#        with_fx(:slicer, phase: 0.025, probability: 0.5, mix: m) do
-         sample Frag[/hat/i, Range.new(0,(ring 2,4).tick(:cr))].tick(:d), start: rrand(0.0,0.1), rate: 1.0, amp: 2.0
+  with_fx((knit :none, 3, :echo, 1, :none, 4).tick(:fx), decay: 0.5, phase: 0.25, mix: 0.4){
+    #sample Fraz[/kick/i,[0,0]].tick(:kick), cutoff: (knit 130, 1, 115, 3).tick(:kcut)
+    sample Dust[/TDDC_Voice_Hit/,16], amp: 1.0, cutoff: (knit 85,3,90,1).tick(:thing), rate: -1
+    #sample Fraz[/kick/,0] if (spread 3, 8, rotate: 1).tick(:pat)
+
+  }
+  #sample "/Users/josephwilk/Dropbox/Music/samples/Chillstep\ Vocals/PBB_CHILLSTEP_VOCALS_WAV/WAV/VOCAL_HITS_AND_FX/BREATH_NOISES/CV_FE_BREATH_029.wav", amp: 0.1, rate: 0.8
+
+  4.times{sync :apeg}
+  #sample Fraz[/kick/i,[1,2]].tick(:kick2), cutoff: (knit 0,1,120,1).tick(:kick2) 
+   #sample Dust[/Voice_Hit/,5], amp: 1.0, cutoff: 70
+      
+   #sample Dust[/TDDC_Voice_Hit/,16], amp: 1.0, cutoff: (knit 85,3,100,1).tick(:thing), rate: 1
+   #sample Sop[/F#/,4], amp: 4.5, cutoff: 100
+
+  with_fx(:reverb, room: 0.5, mix: 0.3, phase: 0.025){
+  with_fx(:slicer, probability: 0.5){
+#  sample Fraz[/snap/i,[0,0,0,0,1,1,1,1]].tick(:snap), amp: 0.2, rate: rrand(1.01,0.99), cutoff: (knit 100,2, 100,1, 110,1).tick(:cutoff)
+  }
+  }
+
+  if(d_idx % 8 == 0)
+    sample Vocals[/breath/], amp: 0.1, rate: 1.0, cutoff: 100  
+    sleep 0.25
+    sample Vocals[/breath/], amp: 0.1, rate: 1.0, cutoff: 100
+  end
+
+  4.times{sync :apeg}
+  d_idx+=1
 end
-#        end
-      end
-  sleep 1/8.0}
-  idx+=1
+
+live_loop :texture do
+  sample_and_sleep Mountain[/cracklin/i, 0], rate: 0.9, amp: 0.2                 
 end
 
-# F♯, G♯, A, B, C♯, D, and E
+live_loop :hats do
+  sync :apeg
+  mix = [0.1,0.2,0.3,0.4,0.5].choose
 
+#  sample (ring Fraz[/kick/,0], Fraz[/kick/,1]).tick(:s),  amp: 2.5  if (spread 1, 4).tick(:d)
+ # sample Dust[/voice/,8],  amp: 2.5  if (spread 1, 8, rotate: 4).look(:d)
+  #sample Dust[/voice/,16], amp: 2.5 if (spread 3, 8).look(:d)
+  #sample Dust[/voice/,14],  amp: 1.2 if (spread 7, 11).look(:d)
 
-#live_loop :perc do
-#  sample Frag[/coil/i,"D",2], amp: 1.0
- # 32.times{sync :arp}
-#  sample Frag[/coil/i,"F#",2], amp: 1.0
- # 32.times{sync :arp}
-#end
+  with_fx(:reverb, mix: mix, room: 0.8){
+  #  sample Frag[/hat/i, Range.new(0,(ring 2,4).tick(:r))].tick(:hats), amp: 0.1, cutoff: (knit 105,3,115,3).tick(:hats)
+     sample Dust[/banton/,0], amp: (knit 1,3,0,1).tick(:k), cutoff: (knit 80,3,100,3).tick(:c)
+  }
+end
