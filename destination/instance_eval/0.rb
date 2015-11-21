978c481d0ea03b3e6f72e90c17fb60c1ef33599b
Joseph Wilk <joe@josephwilk.net>
2015-11-21 21:01:25 +0000

    decay for longer

--- destination/instance_eval/0.rb
+++ destination/instance_eval/0.rb
@@ -103,7 +103,7 @@ live_loop :sop do
   sync :organ
   sleep 16
   if spread(1,4).tick(:sop)
-    with_fx(:echo, room: 1.0, mix: 0.8, damp: 0.5) do |r_fx|
+    with_fx(:echo, room: 1.0, mix: 0.8, damp: 0.5, decay: 8.0) do |r_fx|
       sample Sop[/eh/,/release/, 3..4].tick(:sop), amp: 1.0, cutoff: 100
     end
   end
