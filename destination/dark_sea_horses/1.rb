01c39dd76cb663640b042c082755e3544a344136
Joseph Wilk <joe@josephwilk.net>
2016-01-12 15:38:54 +0000

    new defaults

--- destination/dark_sea_horses/1.rb
+++ destination/dark_sea_horses/1.rb
@@ -1,10 +1,10 @@
-#graphics setup
 #Defaults
 shader :shader, "wave.glsl", "bits.vert", "points", 10000
 shader [:iR, :iB, :iG], 3.0
-shader :iForm, 0.1
+shader :iForm, 0.01
 shader :iDir, 1.0
-shader :iMotion, 0.01
-shader :iWave, 0.5
-shader :iScale, 0.01
-shader :iSize, 5
+shader :iMotion, 0.002
+shader :iWave, 0.01
+shader :iSize, 100.9
+shader :iPointSize, 0.2
+shader :iDistort, 0.005
