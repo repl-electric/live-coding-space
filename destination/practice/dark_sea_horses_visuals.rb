9bc051590b5b6901f771ceb46fd75f7f9aa93fdc
Joseph Wilk <joe@josephwilk.net>
2016-01-12 22:46:37 +0000

    actually do suspension
    Nice touch with beep. Needs to be subtle to work

--- destination/practice/dark_sea_horses_visuals.rb
+++ destination/practice/dark_sea_horses_visuals.rb
@@ -29,9 +29,8 @@ live_loop :warm do
   #I just learnt this...
 
   #Grab the next chord not playing yet.
-  dis_note = data.look(offset: 1)[2] #unstable note maybe...
+  dis_note = data.look(:main, offset: 1)[0] #unstable note maybe...
   #Try the 2nd note of the chord
-
 
   with_fx(:reverb, room: 0.6, mix: 0.4, damp: 0.5) do |r_fx|
     with_transpose 0 do
@@ -45,7 +44,7 @@ live_loop :warm do
     sleep 6
     with_transpose 0 do
       with_fx(:reverb, room: 1.0, mix: 0.3, damp: 0.5) do |r_fx|
-        #synth :dark_ambience, note: (ring dis_note, _, _).tick(:not_always), attack: 0.01, decay: 8.0, amp: 1.0, cutoff: 50
+        synth :beep, note: (ring dis_note, _, _).tick(:not_always), attack: 1.0, decay: 4.0, amp: 0.1
       end
     end
 
@@ -59,19 +58,18 @@ live_loop :warm do
   end
 
   with_fx :pitch_shift, mix: 0.1 do
-
     with_transpose -24 do
-      # bass_dsh =  synth :dark_sea_horn, note: notes[0], decay: 2, cutoff: 130, amp: 0.05, note_slide: 0.01
+      bass_dsh =  synth :dark_sea_horn, note: notes[0], decay: 2, cutoff: 130, amp: 0.01, note_slide: 0.01
     end
 
-    # dsh = synth :dark_sea_horn, note: notes[1], decay: 8, cutoff: 60, amp: 0.1, note_slide: 0.01
+    dsh = synth :dark_sea_horn, note: notes[1], decay: 8, cutoff: 60, amp: 0.1, note_slide: 0.01
     sleep 1
-    #dsh2 = synth :dark_sea_horn, note: notes[2], decay: 7, cutoff: 50, amp: 0.1, note_slide: 0.01
+    dsh2 = synth :dark_sea_horn, note: notes[2], decay: 7, cutoff: 50, amp: 0.1, note_slide: 0.01
     sleep 1
-    # dsh3 = synth :dark_sea_horn, note: notes[0], decay: 6, cutoff: 50, amp: 0.1, note_slide: 0.01
+    dsh3 = synth :dark_sea_horn, note: notes[0], decay: 6, cutoff: 50, amp: 0.1, note_slide: 0.01
 
     if notes.length > 4
-      # dsh4 = synth :dark_sea_horn, note: notes[-1], decay: 6, cutoff: 80, amp: 0.1, note_slide: 0.01
+      dsh4 = synth :dark_sea_horn, note: notes[-1], decay: 6, cutoff: 80, amp: 0.1, note_slide: 0.01
     end
   end
 
@@ -83,7 +81,7 @@ end
 
 live_loop :cracklin do
   with_fx(:pitch_shift, time_dis: 0.9) do
-    sample_and_sleep Mountain[/cracklin/], rate: 0.9, amp: 0.4, cutoff: 130
+    sample_and_sleep Mountain[/cracklin/], rate: 0.9, amp: 0.1, cutoff: 60
   end
 end
 
@@ -108,6 +106,6 @@ live_loop :heart, sync: :warm do
   # shader :decay, :iKick, 1.0
   # shader :decay, :iBeat, 1.0
   #  sample Fraz[/kick/,[0,0]].tick(:sample), cutoff: 50
-  sample Abstract[/perc/,/loop/,[0,0]].tick(:sample), cutoff: 60, amp: 0.5, beat_stretch: 4
+  sample Fraz[/interference/,/c#/,0], amp: 0.01, beat_stretch: 16
   sleep 4
 end
