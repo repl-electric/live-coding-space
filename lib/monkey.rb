c04afc790ef901e683f19654cf9daf316fb69e6c
Joseph Wilk <joe@josephwilk.net>
2016-08-04 22:40:47 +0100

    filter + reject

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -5,4 +5,10 @@ class SonicPi::Core::RingVector
   def shuffle
     SonicPi::Core::RingVector.new(self.to_a.shuffle)
   end
+  def filter(&fun)
+    SonicPi::Core::RingVector.new(self.to_a.filter(&fun))
+  end
+  def reject(&fun)
+    SonicPi::Core::RingVector.new(self.to_a.reject(&fun))
+  end
 end
