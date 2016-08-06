c8f98673839a261b3f3d7ad325db0c0d3b1aaa17
Joseph Wilk <joe@josephwilk.net>
2016-08-06 13:00:54 +0100

    force connection

--- lib/dsp.rb
+++ lib/dsp.rb
@@ -3,8 +3,10 @@ module Dsp
   def self.query(sql)
     SonicPi::Core::RingVector.new(Dsp.connection.query(sql,:symbolize_keys => true).to_a)
   end
-  
   def self.connection()
     @@connection ||= Mysql2::Client.new(:host => "localhost", :username => "root", :database => "repl_electric_samples")
+  end
+  def self.connect!()
+    @@connection = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "repl_electric_samples")
   end
 end
