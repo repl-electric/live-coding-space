47c8ddb6ac35f078f0a14d58c86075d1f2c5a2c0
Joseph Wilk <joe@josephwilk.net>
2016-08-06 21:38:58 +0100

    last minute hacks. Always allow recovery of visuals

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -15,3 +15,28 @@ class SonicPi::Core::RingVector
     SonicPi::Core::RingVector.new(self.to_a.select(&fun))
   end
 end
+module Shaderview
+  def self.voc
+    shader :shader, "voc.glsl", "bits.vert", "points", 10000
+    shader :iStarMotion, 0.8
+    shader :iSpaceLight, 0.4
+    shader :iStarLight, 0.8
+    shader [:iR, :iB, :iG], 3.0
+    shader :iForm, 1.0
+    shader :iMotion, 0.1
+    shader :iSize,10.1
+    shader :iDistort, 100.0
+  end
+  def self.wave
+    shader :shader, "wave.glsl", "bits.vert", "points", 10000
+    shader [:iR, :iB, :iG], 3.0
+    shader :iForm, 0.01
+    shader :iDir, 1.0
+    shader :iMotion, 0.002
+    shader :iWave, 0.01
+    shader :iSize, 100.9
+    shader :iPointSize, 0.2
+    shader :iDistort, 0.005
+    shader :iWave, 0.0
+  end
+end
