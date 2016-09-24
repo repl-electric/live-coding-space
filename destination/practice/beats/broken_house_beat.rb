01f562adde232e3a61030e906d621b28935e415c
Joseph Wilk <joe@josephwilk.net>
2016-09-24 20:04:21 +0100

    Broken house beat

---
+++ destination/practice/beats/broken_house_beat.rb
@@ -0,0 +1,91 @@
+use_bpm 115
+#BROKEN HOUSE
+#https://www.attackmagazine.com/technique/beat-dissected/broken-house-beat/
+live_loop :drums do
+  smp Frag[/kick/,0]
+  smp Frag[/hat/,1]
+  sleep 3/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  
+  sample Fraz[/snap/,0]
+  smp Frag[/hat/,1]
+  
+  sleep 1/4.0
+  smp Frag[/kick/,0]
+  smp Junk[/wood/,0]
+  
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/,1]
+  smp Frag[/kick/,0]
+  sleep 1/4.0
+  
+  
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  smp Junk[/wood/,0]
+  smp Frag[/kick/,0]
+  smp Frag[/hat/,12]
+  
+  sleep 1/4.0
+  
+  smp Frag[/hat/,1]
+  sample Fraz[/snap/,0]
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  
+  
+  smp Frag[/kick/,0]
+  smp Frag[/hat/,1]
+  
+  sleep 1/4.0
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  
+  
+  
+  sample Fraz[/snap/,0]
+  smp Frag[/hat/,1]
+  
+  sleep 1/4.0
+  smp Frag[/kick/,0]
+  smp Junk[/wood/,0]
+  smp Frag[/hat/,1]
+  
+  sleep 2/4.0
+  smp Frag[/hat/,1]
+  smp Frag[/kick/,0]
+  sleep 1/4.0
+  
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  smp Frag[/kick/,0]
+  smp Junk[/wood/,0]
+  
+  sleep 1/4.0
+  
+  sample Fraz[/snap/,0]
+  smp Frag[/hat/,1]
+  
+  sleep 1/4.0
+  sleep 1/4.0
+  
+  smp Frag[/kick/,0]
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+  smp Frag[/hat/,0]
+  sleep 1/4.0
+end
