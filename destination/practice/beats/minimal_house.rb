7696675fce9091187f0f2666491703e0bfe44b04
Joseph Wilk <joe@josephwilk.net>
2016-09-24 20:50:02 +0100

    Minimal house

---
+++ destination/practice/beats/minimal_house.rb
@@ -0,0 +1,87 @@
+use_bpm 123
+#Minimal house
+set_volume! 1.0
+#https://www.attackmagazine.com/technique/beat-dissected/minimal-house/
+live_loop :drums do
+  
+  smp Mountain[/subkick/,0]
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15], amp: 1.0
+  sleep 1/4.0
+  sleep 1/4.0
+  
+  smp Mountain[/subkick/,0]
+  smp Mountain[/snare/]
+  smp Mountain[/clap/]
+  smp MagicDust[/_HI/,14]
+  sleep 1/4.0
+  smp MagicDust[/_HI/,16]
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15]
+  sleep 1/4.0
+  sleep 1/4.0
+  
+  smp Mountain[/subkick/,0]
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15]
+  sleep 1/4.0
+  sleep 1/4.0
+  
+  smp Mountain[/subkick/,0]
+  smp Mountain[/snare/]
+  smp Mountain[/clap/]
+  smp MagicDust[/_HI/,14]
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15]
+  smp MagicDust[/_HI/,16]
+  sleep 1/4.0
+  smp MagicDust[/_HI/,17]
+  sleep 1/4.0
+  
+  smp Mountain[/subkick/,0]
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15]
+  sleep 1/4.0
+  sleep 1/4.0
+  
+  smp Mountain[/subkick/,0]
+  smp Mountain[/snare/]
+  smp Mountain[/clap/]
+  smp MagicDust[/_HI/,14]
+  sleep 1/4.0
+  smp MagicDust[/_HI/,16]
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15]
+  sleep 1/4.0
+  sleep 1/4.0
+  
+  smp Mountain[/subkick/,0]
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15]
+  sleep 1/4.0
+  sleep 1/4.0
+  
+  smp Mountain[/subkick/,0]
+  smp Mountain[/snare/]
+  smp Mountain[/clap/]
+  sleep 1/4.0
+  sleep 1/4.0
+  smp Frag[/hat/]
+  smp MagicDust[/_HI/,15]
+  smp MagicDust[/_HI/,16]
+  sleep 1/4.0
+  smp MagicDust[/_HI/,17]
+  sleep 1/4.0
+end
