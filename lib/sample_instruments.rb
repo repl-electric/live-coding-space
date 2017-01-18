a17f9e9596b44cd3ced93c492f8ac09deb500287
Joseph Wilk <joe@josephwilk.net>
2017-01-18 12:12:57 +0000

    Adding harp + vocal slicers

---
+++ lib/sample_instruments.rb
@@ -0,0 +1,21 @@
+module Harp
+  def self.slice(n, size: 1/4.0)
+    @harp_cache ||= {}
+    n = n.to_s.gsub(/s/,"#")
+    if !@harp_cache.has_key?(n)
+      @harp_cache[n] = NoteSlices.find(note: n, max: size, pat: "harp").take(64)
+    end
+    @harp_cache[n]
+  end
+end
+
+module Vocal
+  def self.slice(n, size: 1/4.0)
+    @vocal_cache ||= {}
+    n = n.to_s.gsub(/s/,"#")
+    if !@vocal_cache.has_key?(n)
+      @vocal_cache[n] = NoteSlices.find(note: n, max: size, pat: "sop|alto|bass").take(64)
+    end
+    @vocal_cache[n]
+  end
+end
